ecf7a53bf1a8efa70e2a9156361ed34881bcc3b8
taubut <taubut@gmail.com>
2016-04-13 21:27:58 -0400

    Added files via upload

---
+++ PlayTBC.com Font Color.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         PlayTBC.com Font Color
+// @namespace    reddit.com/user/taubut
+// @version      0.1
+// @description  changing the font color to be readable
+// @author       Taubut
+// @match        http://playtbc.com/
+// @grant        none
+// ==/UserScript==
+
+$(".body").css("color", "#E4E4E4");
